1d9da54421f434323064cced439e672843922de9
zhangchen <windyzj@163.com>
2020-07-15 17:27:31 +0800

    扫描路径

--- gmall0213-logger/src/main/java/com/atguigu/gmall0213/logger/Gmall0213LoggerApplication.java
+++ gmall0213-logger/src/main/java/com/atguigu/gmall0213/logger/Gmall0213LoggerApplication.java
@@ -2,8 +2,10 @@ package com.atguigu.gmall0213.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atguigu.gmall0213.logger.controller")
 public class Gmall0213LoggerApplication {
 
     public static void main(String[] args) {
