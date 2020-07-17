62ba27aba9d524a38f0d2426ceaef17654b8aeb4
zhangchen <windyzj@163.com>
2020-07-17 17:06:11 +0800

    去重思路

--- gmall0213-realtime/src/main/scala/com/atguigu/gmall0213/realtime/app/DauApp.scala
+++ gmall0213-realtime/src/main/scala/com/atguigu/gmall0213/realtime/app/DauApp.scala
@@ -16,6 +16,30 @@ object DauApp {
 
       recordInputDstream.map(_.value()).print(1000)
 
+
+    // 1  去重  把启动日志 只保留首次启动 （日活）
+    //  redis去重
+    // 去重  type:set  sadd     type:String setnx  既做了判断 又做了写入
+    //set 当日的签到记录保存在一个set中   便于管理  整体查询速度快
+    //string  把当天的签到记录 散列存储在redis   当量级巨大时 可以利用集群 做分布式
+    recordInputDstream.filter{record=>
+      val jsonString: String = record.value()
+       // 把json变成对象map jsonObject  case class
+
+      // 取出来 mid
+      //    用mid 保存一个清单 （set）
+      // redis  type ? set   key?  dau:2020-07-17   value ? mid
+      //  用sadd 执行
+      // 判断返回值 1或0  1 保留数据 0 过滤掉
+
+      null
+    }
+    //得到一个过滤后的Dstream
+
+    //写到es 中
+
+
+
       ssc.start()
       ssc.awaitTermination()
   }
